7acb1f0682f7453af1b0d91a9f45d3d20deb0fae
AaxX92 <chinaswlcx@outlook.com>
2020-11-21 14:10:31 +0800

    11

---
+++ GCD&LCM/main.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import GCD_LCM
+
+window = tk.Tk()
+window.title("my window")
+window.geometry("350x200")
+
+
+
+#检查输入的内容是否为数字
+def correct(inp):
+    if inp.isdigit():
+        return True
+    elif inp == "":
+        return True
+    else:
+        return False
+
+reg = window.register(correct)
+
+
+
+
+tab = ttk.Notebook(window)
+
+
+frame1 = tk.Frame(tab)
+tab1 = tab.add(frame1, text = "最大公因数")
+
+f1_inpbox1 = tk.Entry(frame1)
+f1_inpbox1.pack()
+f1_inpbox2 = tk.Entry(frame1)
+f1_inpbox2.pack()
+f1_inpbox3 = tk.Entry(frame1)
+f1_inpbox3.pack()
+
+f1_inpbox1.config(validate="key", validatecommand=(reg,"%P"))
+f1_inpbox2.config(validate="key", validatecommand=(reg,"%P"))
+f1_inpbox3.config(validate="key", validatecommand=(reg,"%P"))
+
+def start_count1():
+    num1 = GCD_LCM.my_prime_factorization(int(f1_inpbox1.get()))
+    num2 = GCD_LCM.my_prime_factorization(int(f1_inpbox2.get()))
+    num3 = GCD_LCM.my_prime_factorization(int(f1_inpbox3.get()))
+    a =  GCD_LCM.gcd_count(num1,num2,num3)
+    label1.config(text="最大公因数为：" + str(a))
+
+f1_btn1 = tk.Button(frame1, text="开始计算", command=start_count1)
+f1_btn1.pack()
+
+label1 = tk.Label(frame1, text="")
+label1.pack()
+
+
+
+frame2 = tk.Frame(tab)
+tab2 = tab.add(frame2, text = "最小公倍数")
+
+f2_inpbox1 = tk.Entry(frame2)
+f2_inpbox1.pack()
+f2_inpbox2 = tk.Entry(frame2)
+f2_inpbox2.pack()
+f2_inpbox3 = tk.Entry(frame2)
+f2_inpbox3.pack()
+
+f2_inpbox1.config(validate="key", validatecommand=(reg,"%P"))
+f2_inpbox2.config(validate="key", validatecommand=(reg,"%P"))
+f2_inpbox3.config(validate="key", validatecommand=(reg,"%P"))
+
+def start_count2():
+    num1 = GCD_LCM.my_prime_factorization(int(f2_inpbox1.get()))
+    num2 = GCD_LCM.my_prime_factorization(int(f2_inpbox2.get()))
+    num3 = GCD_LCM.my_prime_factorization(int(f2_inpbox3.get()))
+    a =  GCD_LCM.lcm_count(num1,num2,num3)
+    label2.config(text="最小公倍数为：" + str(a))
+
+f2_btn1 = tk.Button(frame2, text="开始计算", command=start_count2)
+f2_btn1.pack()
+
+label2 = tk.Label(frame2, text="")
+label2.pack()
+
+
+tab.pack(expand=True, fill=tk.BOTH)
+
+
+
+window.mainloop()
